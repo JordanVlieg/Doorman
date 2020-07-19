327792baddab7274d0cccbc4612adddd90db0cad
Jordan Vlieg <jordanvlieg@gmail.com>
2020-07-19 14:24:51 -0700

    Validate all requess come from twilio, disable validation in dev

--- main.go
+++ main.go
@@ -14,41 +14,42 @@ import (
 )
 
 func beepFile(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./beep.mp3")
+	if validateReqFromTwilio(w, r) {
+		http.ServeFile(w, r, "./beep.mp3")
+	}
 }
 
 func knock(w http.ResponseWriter, r *http.Request) {
-	play := types.Play{Message: getFullURI("BEEP")}
-	gather := types.Gather{Action: os.Getenv("ATTEMPT_ENTRY"), Method: "POST", NumDigits: "4", Timeout: "7", ActionOnEmptyResult: "false", Say: os.Getenv("MAIN_WELCOME"), Play: &play}
-	twiml := types.TwiML{Gather: &gather}
-	twilioWriter(twiml, w)
+	if validateReqFromTwilio(w, r) {
+		play := types.Play{Message: getFullURI("BEEP")}
+		gather := types.Gather{Action: os.Getenv("ATTEMPT_ENTRY"), Method: "POST", NumDigits: "4", Timeout: "7", ActionOnEmptyResult: "false", Say: os.Getenv("MAIN_WELCOME"), Play: &play}
+		twiml := types.TwiML{Gather: &gather}
+		twilioWriter(twiml, w)
+	}
 }
 
 func attemptEntry(w http.ResponseWriter, r *http.Request) {
-	err := twilio_go.ValidateIncomingRequest(os.Getenv("BASE_URI"), os.Getenv("AUTH_TOKEN"), r)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusForbidden)
-		return
-	}
+	if validateReqFromTwilio(w, r) {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-	err = r.ParseForm()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+		play := types.Play{Digits: doorCodeToTwilioTone(os.Getenv("BUZZ_CODE"))}
+		var twiml types.TwiML
 
-	play := types.Play{Digits: doorCodeToTwilioTone(os.Getenv("BUZZ_CODE"))}
-	var twiml types.TwiML
-	switch password := r.Form["Digits"][0]; password {
-	case os.Getenv("DELIVERY_PASSWORD"):
-		twiml = types.TwiML{Say: os.Getenv("DELIVERY_WELCOME"), Play: &play}
-	case os.Getenv("PERSONAL_PASSWORD"):
-		twiml = types.TwiML{Say: os.Getenv("PERSONAL_WELCOME"), Play: &play}
-	default:
-		twiml = types.TwiML{Say: os.Getenv("DENIED_MESSAGE")}
-	}
+		switch password := r.Form["Digits"][0]; password {
+		case os.Getenv("DELIVERY_PASSWORD"):
+			twiml = types.TwiML{Say: os.Getenv("DELIVERY_WELCOME"), Play: &play}
+		case os.Getenv("PERSONAL_PASSWORD"):
+			twiml = types.TwiML{Say: os.Getenv("PERSONAL_WELCOME"), Play: &play}
+		default:
+			twiml = types.TwiML{Say: os.Getenv("DENIED_MESSAGE")}
+		}
 
-	twilioWriter(twiml, w)
+		twilioWriter(twiml, w)
+	}
 }
 
 func doorCodeToTwilioTone(code string) string {
@@ -73,6 +74,17 @@ func twilioWriter(twiml types.TwiML, w http.ResponseWriter) {
 
 	w.Header().Set("Content-Type", "application/xml")
 	w.Write(x)
+}
+
+func validateReqFromTwilio(w http.ResponseWriter, r *http.Request) bool {
+	if os.Getenv("DEVELOPMENT") != "true" {
+		err := twilio_go.ValidateIncomingRequest(os.Getenv("BASE_URI"), os.Getenv("AUTH_TOKEN"), r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
